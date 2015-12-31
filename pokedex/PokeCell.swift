c95be7852252269a209bfb3a6433582b73b6762e
facecat <facecat10002gmail.com>
2015-12-31 21:11:58 +0800

    add custom pokecell and almost finish UI of 1st scene

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by 邱国邦 on 15/12/31.
+//  Copyright © 2015年 FaceCat. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    private var _poke: Pokemon!
+    
+    func configureCell(poke: Pokemon) {
+        _poke = poke
+        
+        mainImg.image = _poke.pokemonImg
+        nameLbl.text = _poke.name
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+
+    
+
+}
--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
 
+    var pokemon = [Pokemon]()
+    
+    @IBOutlet weak var collectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionview.dataSource = self
+        collectionview.delegate = self
+        
+        let p1 = Pokemon(name: "Who Am I?", pokedexId: "200")
+        let p2 = Pokemon(name: "I am NOBODY", pokedexId: "300")
+        let p3 = Pokemon(name: "OH SHOOT", pokedexId: "400")
+        pokemon.append(p1)
+        pokemon.append(p2)
+        pokemon.append(p3)
+        collectionview.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    //MARK: UICollectionViewDelegate & DataSource
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemon.count
     }
 
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        if let cell = collectionview.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
+            let poke = pokemon[indexPath.row]
+            cell.configureCell(poke)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
 
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(106, 106)
+    }
 }
+
+
+
+
 
